26eaa92e770e9a2867bb0cf477540d7cc30d5012
GetRektd <lgibb24@aoiths.org>
2023-11-22 14:05:31 -0500

    Esacpe Roomy

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,11 +9,19 @@ public class SpawnManager : MonoBehaviour
 
     private float xRange = 2;
 
-    private float spawnPosZ = 4;
+    private float spawnPosZ = .5f;
+
     
     private float spawnInterval = 1.2f;
 
     private float startDelay = 3;
+
+    int counter = 10;
+
+    public GameObject keyPrefab;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,17 +31,31 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
+
+
+
     }
 
     void SpawnRandomBalls ()
 
     {
+         if (counter < 1) 
+        {
+            CancelInvoke();
+            Instantiate(keyPrefab, new Vector3(-2, .5f, .17f), keyPrefab.transform.rotation );
+        }
 
         int sphereIndex =  Random.Range(0, enemyPrefabs.Length);
-        Vector3 spawnpos = new Vector3(Random.Range (-xRange, xRange), spawnPosZ);
+        Vector3 spawnpos = new Vector3(Random.Range (-xRange, xRange), 0, -4);
         Instantiate(enemyPrefabs[sphereIndex], spawnpos, enemyPrefabs[sphereIndex].transform.rotation);
+        counter --;
     }
+
+    
+
+
+
 
 }
 
